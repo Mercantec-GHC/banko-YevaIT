0587c38b00503a34c9f835a575f8137664e77257
Yevheniia Zhygalova <119417215+YevaIT@users.noreply.github.com>
2023-10-25 14:33:34 +0200

    Made plates, input and message logic

---
+++ Banko/InputController.cs
@@ -0,0 +1,37 @@
+﻿namespace Banko;
+
+
+
+
+public class InputController
+{
+    TerminalRender render;
+    public InputController() { 
+        render = new TerminalRender();
+    }
+    public int ReadNum(string message, int min = 1, int max = 90)
+    {
+        int result;
+        string? rawInput;
+
+        while (true)
+        {
+            render.ShowMessage(message);
+            rawInput = Console.ReadLine();
+            bool isNum = int.TryParse(rawInput, out result);
+            if (isNum && result >= min && result <= max)
+            {
+             
+                break;
+                
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you entered an invalid number, please try again");
+            }
+
+        }
+        return result;
+
+    }
+};
---
+++ Banko/TerminalRender.cs
@@ -0,0 +1,24 @@
+﻿namespace Banko;
+
+public class TerminalRender
+{
+
+ 
+
+    public void ShowPlate(string[,] plate)
+    {
+        Console.WriteLine("┌──┬──┬──┬──┬──┬──┬──┬──┬──┐");
+        Console.WriteLine($"│{(plate[0, 0])}│{(plate[0, 1])}│{(plate[0, 2])}│{(plate[0, 3])}│{(plate[0, 4])}│{(plate[0, 5])}│{(plate[0, 6])}│{(plate[0, 7])}│{(plate[0, 8])}│");
+        Console.WriteLine("├──┼──┼──┼──┼──┼──┼──┼──┼──┤");
+        Console.WriteLine($"│{plate[1, 0]}│{plate[1, 1]}│{plate[1, 2]}│{plate[1, 3]}│{plate[1, 4]}│{plate[1, 5]}│{plate[1, 6]}│{plate[1, 7]}│{plate[1, 8]}│");
+        Console.WriteLine("├──┼──┼──┼──┼──┼──┼──┼──┼──┤");
+        Console.WriteLine($"│{plate[2, 0]}│{plate[2, 1]}│{plate[2, 2]}│{plate[2, 3]}│{plate[2, 4]}│{plate[2, 5]}│{plate[2, 6]}│{plate[2, 7]}│{plate[2, 8]}│");
+        Console.WriteLine("└──┴──┴──┴──┴──┴──┴──┴──┴──┘");
+    }
+
+    public void ShowMessage(string message) {
+        Console.WriteLine(message);
+    }
+}
+
+
